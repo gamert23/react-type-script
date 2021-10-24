6cc33fee402a7aa97af2135c052aa2daacd38193
Ratchanon <ratchanon.p@cjworx.com>
2021-10-24 22:27:43 +0700

    Add handling props and events

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import List from './components/List';
+import AddToList from './components/AddToList';
 
-interface IState {
+export interface IState {
   people: {
     name: string,
     age: number,
@@ -13,7 +15,14 @@ interface IState {
 
 function App() {
   const [number, setNumber] = useState<number | string>(5);
-  const [people, setPeople] = useState<IState["people"]>([])
+  const [people, setPeople] = useState<IState["people"]>([
+    {
+      name: 'Mason Mount',
+      age: 22,
+      url: 'https://ga.lnwfile.com/aviu3j.jpg',
+      note: 'Hatrick Hero'
+    }
+  ])
 
   const changeNumber = () => {
     setNumber("10")
@@ -22,6 +31,8 @@ function App() {
   return (
     <div className="App">
       <h1> People Invited to my Party</h1>
+      <List people={people} />
+      <AddToList people={people} setPeople={setPeople} />
     </div>
   );
 }
